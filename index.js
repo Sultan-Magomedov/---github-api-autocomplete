d95220a366d7c96852d202d12326ec76c5fb933a
Sultan-Magomedov <Sultan_magomedov_0108@mail.ru>
2025-06-03 19:08:17 +0300

    git commit -m second

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const input = document.querySelector(".search");
 const dropdown = document.querySelector(".dropdown");
 const repositories = document.querySelector(".repositoryes");
+let data;
 
 const debounce = (fn, debounceTime) => {
   let id = null;
@@ -13,51 +14,50 @@ const debounce = (fn, debounceTime) => {
 };
 
 const getRepositories = async function () {
-  try {
-    const request = await fetch(
-      `https://api.github.com/search/repositories?q=${input.value}&sort=stars&order=desc&per_page=5`
-    );
-    const resolve = await request.json();
-    let dropdownHtml = "";
-    for (item of resolve.items) {
-      dropdownHtml += `<li class="dropdown__item">${item.name}</li>`;
-    }
-    dropdown.innerHTML = dropdownHtml;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const createRepository = async function (event) {
-  if (event.target.classList.contains("dropdown__item")) {
+  if (input.value.trim().length !== 0) {
     try {
       const request = await fetch(
         `https://api.github.com/search/repositories?q=${input.value}&sort=stars&order=desc&per_page=5`
       );
-      const resolve = await request.json();
-      const repos = resolve.items;
-      const filteredRepositories = repos.filter(
-        (rep) => rep.name === event.target.textContent
-      );
-      const repositoryItem = ` <li class="repositoryes__item">
-    <div class="wrap">
-    <p>Name: ${filteredRepositories[0].name}</p>
-    <p>Owner: ${filteredRepositories[0].owner.login}</p>
-    <p>Stars: ${filteredRepositories[0].stargazers_count}</p>
-  </div>
-    <button class="bts"></button>
-      </li>`;
-      repositories.insertAdjacentHTML("afterbegin", repositoryItem);
-      input.value = "";
-      dropdown.innerHTML = "";
+      data = await request.json();
+      console.log(data);
+      let dropdownHtml = "";
+      for (item of data.items) {
+        dropdownHtml += `<li class="dropdown__item">${item.name}</li>`;
+      }
+      dropdown.innerHTML = dropdownHtml;
     } catch (error) {
       console.log(error);
     }
+  } else dropdown.innerHTML = "";
+};
+
+const createRepository = function (event) {
+  if (event.target.classList.contains("dropdown__item")) {
+    const repos = data.items;
+    const filteredRepositories = repos.filter(
+      (rep) => rep.name === event.target.textContent
+    );
+    const repositoryItem = ` <li class="repositoryes__item">
+        <div class="wrap">
+        <p>Name: ${filteredRepositories[0].name}</p>
+        <p>Owner: ${filteredRepositories[0].owner.login}</p>
+        <p>Stars: ${filteredRepositories[0].stargazers_count}</p>
+        </div>
+        <button class="bts"></button>
+        </li>`;
+
+    repositories.insertAdjacentHTML("afterbegin", repositoryItem);
+    input.value = "";
+    dropdown.innerHTML = "";
   }
+};
+const deleteRepository = function (event) {
   if (event.target.classList.contains("bts")) {
     event.target.closest(".repositoryes__item").remove();
   }
 };
 
 input.addEventListener("input", debounce(getRepositories, 500));
-document.addEventListener("click", createRepository);
+dropdown.addEventListener("click", createRepository);
+repositories.addEventListener("click", deleteRepository);
